174b223b5047988aa7523a4522ade446ada0e43a
Roothebear <rupert.dasilva@gmail.com>
2022-02-14 14:45:05 +0000

    tests for patch-article endpoint added

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -59,7 +59,7 @@ describe("/api/topics", () => {
 describe("/api/articles", () => {
   // GET testing
   describe("GET /api/articles/:article_id", () => {
-    test("status: 200 - responds with article object", () => {
+    test("status: 200 - responds with article", () => {
       return request(app)
         .get("/api/articles/1")
         .expect(200)
@@ -86,6 +86,66 @@ describe("/api/articles", () => {
         .expect(404)
         .then(({ body: { msg } }) => {
           expect(msg).toBe("no article with this id exists");
+        });
+    });
+  });
+  describe("PATCH /api/articles/:article:_id", () => {
+    // PATCH testing
+    test("status:200, responds with the updated article while ignoring any keys other than inc_votes", () => {
+      const articleUpdate = {
+        inc_votes: 12,
+        irrelevant_key: "something irrelevant",
+      };
+      return request(app)
+        .patch("/api/articles/1")
+        .send(articleUpdate)
+        .expect(200)
+        .then((response) => {
+          expect(response.body.article).toEqual({
+            article_id: 1,
+            title: "Living in the shadow of a great man",
+            topic: "mitch",
+            author: "butter_bridge",
+            body: "I find this existence challenging",
+            created_at: expect.any(String),
+            votes: 112,
+          });
+        });
+    });
+    test("status:400, no inc_votes on request body", () => {
+      const articleUpdate = {
+        irrelevant_key: "something irrelevant",
+      };
+      return request(app)
+        .patch("/api/articles/2")
+        .send(articleUpdate)
+        .expect(400)
+        .then(({ body: { msg } }) => {
+          expect(msg).toBe("null value in body violates not-null constraint");
+        });
+    });
+    test("status:404 invalid inc_votes provided (string)", () => {
+      const articleUpdate = {
+        inc_votes: "a string",
+      };
+      return request(app)
+        .patch("/api/articles/3")
+        .send(articleUpdate)
+        .expect(400)
+        .then(({ body: { msg } }) => {
+          expect(msg).toBe(`invalid input syntax for type integer`);
+        });
+    });
+    test("status:404 invalid inc_votes provided (boolean)", () => {
+      const articleUpdate = {
+        inc_votes: false,
+      };
+      return request(app)
+        .patch("/api/articles/3")
+        .send(articleUpdate)
+        .expect(400)
+        .then(({ body: { msg } }) => {
+          expect(msg).toBe(`invalid input syntax for type integer`);
         });
     });
   });
