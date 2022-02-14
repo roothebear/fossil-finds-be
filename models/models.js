25be00ea4bdd612a32ef941d945b8626fa867785
Roothebear <rupert.dasilva@gmail.com>
2022-02-14 12:53:58 +0000

    add article-by-id model

--- models/models.js
+++ models/models.js
@@ -7,6 +7,28 @@ exports.selectTopics = (req) => {
 };
 
 
+exports.selectArticleById = (req) => {
+  const { article_id } = req.params;
+  return db
+    .query(
+      `SELECT * FROM articles
+            WHERE articles.article_id = $1;`,
+      [article_id]
+    )
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: "no article with this id exists",
+        });
+      } else {
+        return result.rows[0];
+      }
+    });
+};
+
+
+
 // POST MODELS
 
 // PATCH MODELS
