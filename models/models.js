679163fbb841928c22bef7a33e1ca0e490d7a6ca
Roothebear <rupert.dasilva@gmail.com>
2022-02-15 13:57:18 +0000

    refactor articles endpoint to allow topic, sort_by and order queries, and add relevant tests

--- models/models.js
+++ models/models.js
@@ -6,24 +6,145 @@ exports.selectTopics = (req) => {
   return db.query(`SELECT * FROM topics;`).then((result) => result.rows);
 };
 
-// exports.selectArticles = (req) => {
-//   return db.query(
-//       `SELECT * 
-//       FROM articles
-//       ORDER BY created_at DESC;`
-//   ).then((result) => result.rows);
+// exports.selectArticles = async (req) => {
+//   // destructure optional query parameters, we will deal with each in turn
+//   const { sort_by, order, topic } = req.query;
+
+//   // set up array for query values used
+//   const queryValues = [];
+//   let queryStr = `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.body, articles.created_at, articles.votes, COUNT(comment_id )::INT AS comment_count
+//   FROM articles
+//   FULL JOIN comments ON articles.article_id = comments.article_id`;
+
+//   // deal with topic
+//   if (!(typeof topic === "undefined")) {
+//     queryValues.push(topic);
+//     queryStr += ` WHERE topic = $1`;
+//   }
+
+//   // deal with grouping
+//   queryStr += ` GROUP BY articles.article_id`;
+
+//   // deal with sort_by (alpha-numeric column to sort by)
+//   if (!(typeof sort_by === "undefined")) {
+//     if (
+//       !["title", "topic", "author", "body", "created_at", "votes"].includes(
+//         sort_by
+//       )
+//     ) {
+//       return Promise.reject({
+//         status: 400,
+//         msg: "Error - Invalid sort_by query",
+//       });
+//     }
+//     queryStr += ` ORDER BY ${sort_by}`;
+//   } else {
+//     queryStr += ` ORDER BY created_at`;
+//   }
+
+//   // deal with order
+//   if (!(typeof order === "undefined")) {
+//     if (!["asc", "desc"].includes(order)) {
+//       return Promise.reject({
+//         status: 400,
+//         msg: "Error - Invalid order query",
+//       });
+//     }
+//     queryStr += ` ${order}`;
+//   } else {
+//     queryStr += ` DESC`;
+//   }
+
+//   // add final ';'!
+//   queryStr += `;`;
+
+//   const selectedArticles = db.query(queryStr, queryValues).then((result) => {
+//     console.log("selected Articles: ", result.rows)
+//     result.rows;
+//   });
+
+//   const checkTopicExists = checkTopicExists(topic)
+//   .then((result) => {
+//     return result
+//   })
+
+//   console.log("checkTopicExists: ", checkTopicExists)
+
+//   await Promise.all([selectedArticles, checkTopicExists])
+
+//   return db.query(queryStr, queryValues).then((result) => {
+//     return result.rows;
+//   });
 // };
 
-exports.selectArticles = (req) => {
-  return db
-    .query(
-      `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.body, articles.created_at, articles.votes, COUNT(comment_id )::INT AS comment_count
-      FROM articles
-      FULL JOIN comments ON articles.article_id = comments.article_id
-      GROUP BY articles.article_id
-      ORDER BY created_at DESC;`
-    )
-    .then((result) => result.rows);
+exports.selectArticles = async (req) => {
+  // destructure optional query parameters, we will deal with each in turn
+  const { sort_by, order, topic } = req.query;
+
+  // set up array for query values used
+  const queryValues = [];
+  let queryStr = `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.body, articles.created_at, articles.votes, COUNT(comment_id )::INT AS comment_count 
+  FROM articles 
+  FULL JOIN comments ON articles.article_id = comments.article_id`;
+
+  // deal with topic
+  if (!(typeof topic === "undefined")) {
+    queryValues.push(topic);
+    queryStr += ` WHERE topic = $1`;
+  }
+
+  // deal with grouping
+  queryStr += ` GROUP BY articles.article_id`;
+
+  // deal with sort_by (alpha-numeric column to sort by)
+  if (!(typeof sort_by === "undefined")) {
+    if (
+      !["title", "topic", "author", "body", "created_at", "votes"].includes(
+        sort_by
+      )
+    ) {
+      return Promise.reject({
+        status: 400,
+        msg: "Error - Invalid sort_by query",
+      });
+    }
+    queryStr += ` ORDER BY ${sort_by}`;
+  } else {
+    queryStr += ` ORDER BY created_at`;
+  }
+
+  // deal with order
+  if (!(typeof order === "undefined")) {
+    if (!["asc", "desc"].includes(order)) {
+      return Promise.reject({
+        status: 400,
+        msg: "Error - Invalid order query",
+      });
+    }
+    queryStr += ` ${order}`;
+  } else {
+    queryStr += ` DESC`;
+  }
+
+  // add final ';'!
+  queryStr += `;`;
+
+  return db.query(queryStr, queryValues).then((result) => {
+    if (result.rows.length === 0) {
+      // as we have an empty array of articles in this case, check database for existence of the topic with further query
+      return db
+        .query("SELECT * FROM topics WHERE topics.slug = $1;", [topic])
+        .then((topics) => {
+          if (topics.rows.length === 0) {
+            // if topic does not exist return promise
+            return Promise.reject({ status: 404, msg: "Topic not found" });
+          } else {
+            return result.rows;
+          }
+        });
+    }
+    return result.rows;
+  });
 };
 
 exports.selectArticleById = (req) => {
@@ -59,23 +180,9 @@ exports.selectCommentsByArticleId = (req) => {
       [article_id]
     )
     .then((result) => {
-        return result.rows;
+      return result.rows;
     });
 };
-
-// exports.selectCommentsByArticleId = (req) => {
-//   const { article_id } = req.params;
-//   return db
-//     .query(
-//       `SELECT comments.author, comment_id, comments.votes, comments.body, comments.created_at FROM comments
-//       JOIN articles ON comments.article_id = articles.article_id
-//             WHERE comments.article_id = $1;`,
-//       [article_id]
-//     )
-//     .then((result) => {
-//         return result.rows;
-//     });
-// };
 
 exports.selectUsers = (req) => {
   return db.query(`SELECT * FROM users;`).then((result) => result.rows);
