ec4874d678c5d24f25e95bde4aefa1815f19edc4
Roothebear <rupert.dasilva@gmail.com>
2022-02-15 23:36:54 +0000

    update connection.js and listen.js

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,11 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
+require("dotenv").config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+
+
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
@@ -14,11 +19,6 @@ const config =
         },
       }
     : {};
-
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
-
 
 
 module.exports = new Pool(config);
--- listen.js
+++ listen.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-const { PORT = 9094 } = process.env;
+const PORT = process.env.PORT || 9090;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
